7acb8dcd6987fba1fdd4e78e7396c197c5cf7802
poodlepoodle <chammal97@naver.com>
2022-07-19 13:54:05 +0900

    :sparkles: feat: 로그인 및 회원가입 페이지 라우팅 기능 추가
    login.js, pages/signup 빈 페이지로 라우팅 기능만 추가

---
+++ pages/signup/more.js
@@ -0,0 +1,15 @@
+export default function More() {
+  return (
+    <div>
+      <main>
+        <h1>홈페이지 - 회원 가입 성공 시 추가 정보 입력</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>홈페이지 내 회원 가입 성공 시 추가 정보 입력 임시 화면입니다.</h4>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
