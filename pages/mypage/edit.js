5f1a314eab1909587683dd61ec82789c2a2b9c42
poodlepoodle <chammal97@naver.com>
2022-07-19 13:49:42 +0900

    :sparkles: feat: 마이페이지 라우팅 기능 추가
    pages/mypage 빈 페이지로 라우팅 기능만 추가

---
+++ pages/mypage/edit.js
@@ -0,0 +1,15 @@
+export default function Edit() {
+  return (
+    <div>
+      <main>
+        <h1>마이페이지 - 회원 정보 수정</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>마이페이지 내 회원 정보 수정 임시 화면입니다.</h4>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
---
+++ pages/mypage/index.js
@@ -0,0 +1,15 @@
+export default function MyPage() {
+  return (
+    <div>
+      <main>
+        <h1>마이페이지</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>마이페이지 임시 화면입니다.</h4>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
