aed56a6aeefec7a670450fc1639eccc1f7dbe42e
poodlepoodle <chammal97@naver.com>
2022-07-19 14:00:39 +0900

    :sparkles: feat: 커뮤니티 페이지 라우팅 기능 추가
    pages/community 빈 페이지로 라우팅 기능만 추가

---
+++ pages/community/post/new.js
@@ -0,0 +1,15 @@
+export default function New() {
+  return (
+    <div>
+      <main>
+        <h1>커뮤니티 페이지 - 새로운 글 작성</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>커뮤니티 페이지 내 새로운 글 작성 임시 화면입니다.</h4>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
