38e070296230ba1268e2eede756a8de40189f139
poodlepoodle <chammal97@naver.com>
2022-07-19 14:35:26 +0900

    :sparkles: feat: 상단 네비게이션 바 컴포넌트 생성
    src/components/main/navbar.jsx 생성 및 _app.js 내 포함

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import Navbar from '../src/components/main/navbar';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Navbar />
       <Component {...pageProps} />
     </>
   );
---
+++ src/components/main/navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>상단 네비게이션 바</div>;
+};
+
+export default Navbar;
