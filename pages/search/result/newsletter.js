512722bbdeaf760a2791623d77c2ac1546a6c7ef
poodlepoodle <chammal97@naver.com>
2022-07-19 14:04:06 +0900

    :sparkles: feat: 검색 페이지 라우팅 기능 추가
    pages/search 빈 페이지로 라우팅 기능만 추가

---
+++ pages/search/result/newsletter.js
@@ -0,0 +1,17 @@
+export default function Newsletter() {
+  return (
+    <div>
+      <main>
+        <h1>검색 페이지 - 검색 결과 - 뉴스레터만 필터링</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>
+          검색 페이지 검색 결과에서 뉴스레터 결과만 필터링한 임시 화면입니다.
+        </h4>
+      </main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
