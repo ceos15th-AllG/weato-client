1e3f7e66c9e755806b95672bd2f735cdbeea20f6
poodlepoodle <chammal97@naver.com>
2022-07-23 03:32:03 +0900

    :lipstick: style: 전역 스타일 적용
    기존의 전역 스타일 방식을 Emotion 기반으로 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,28 @@
-import Navbar from '../src/components/main/navbar';
-import '../styles/globals.css';
+/** @jsxImportSource @emotion/react */
+import { Global } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
+import GlobalStyle from '../styles/GlobalStyle';
+
+import Head from 'next/head';
+
+import Navbar from '../src/components/main/Navbar';
+
+const App = ({ Component, pageProps }) => {
   return (
-    <>
+    <div>
+      <Head>
+        <link
+          href="//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <Global styles={GlobalStyle} />
+
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
-}
+};
 
-export default MyApp;
+export default App;
---
+++ styles/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const GlobalStyle = css`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+  }
+`;
+
+export default GlobalStyle;
