a785710e6875fa2312f9a70bb63a23cf3c39cf0b
poodlepoodle <chammal97@naver.com>
2022-07-23 10:17:44 +0900

    :lipstick: style: 폰트 스타일 정리
    디자인시스템에 정리된 대로 styles/FontStyle에 정리

---
+++ styles/FontStyle.jsx
@@ -0,0 +1,66 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export const Display1 = css`
+  font-weight: bold;
+  /* font-size: 32px; */
+  font-size: 24px;
+`;
+
+export const Headline2 = css`
+  font-weight: bold;
+  /* font-size: 28px; */
+  font-size: 21px;
+`;
+
+export const Headline1 = css`
+  font-weight: bold;
+  /* font-size: 24px; */
+  font-size: 18px;
+`;
+
+export const Subhead4 = css`
+  font-weight: bold;
+  /* font-size: 20px; */
+  font-size: 15px;
+`;
+
+export const Subhead3 = css`
+  font-weight: bold;
+  /* font-size: 16px; */
+  font-size: 12px;
+`;
+
+export const Body5 = css`
+  font-weight: medium;
+  /* font-size: 24px; */
+  font-size: 18px;
+`;
+
+export const Body4 = css`
+  font-weight: 300;
+  /* font-size: 24px; */
+  font-size: 18px;
+`;
+
+export const Body3 = css`
+  font-weight: medium;
+  /* font-size: 20px; */
+  font-size: 15px;
+`;
+export const Body2 = css`
+  font-weight: 300;
+  /* font-size: 16px; */
+  font-size: 12px;
+`;
+
+export const Body1 = css`
+  font-weight: medium;
+  /* font-size: 14px; */
+  font-size: 11.5px;
+`;
+
+export const Tag = css`
+  font-weight: medium;
+  font-size: 12px;
+`;
