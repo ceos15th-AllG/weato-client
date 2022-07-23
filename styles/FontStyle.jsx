54bb9cf1eee2362a216189a3466519ee91b40a98
poodlepoodle <chammal97@naver.com>
2022-07-23 11:56:22 +0900

    :lipstick: style: 폰트 스타일 세부 수정
    bold font-weight 900에서 500으로 조정

--- styles/FontStyle.jsx
+++ styles/FontStyle.jsx
@@ -2,31 +2,31 @@
 import { css } from '@emotion/react';
 
 export const Display1 = css`
-  font-weight: bold;
+  font-weight: 500;
   /* font-size: 32px; */
   font-size: 24px;
 `;
 
 export const Headline2 = css`
-  font-weight: bold;
+  font-weight: 500;
   /* font-size: 28px; */
   font-size: 21px;
 `;
 
 export const Headline1 = css`
-  font-weight: bold;
+  font-weight: 500;
   /* font-size: 24px; */
   font-size: 18px;
 `;
 
 export const Subhead4 = css`
-  font-weight: bold;
+  font-weight: 500;
   /* font-size: 20px; */
   font-size: 15px;
 `;
 
 export const Subhead3 = css`
-  font-weight: bold;
+  font-weight: 500;
   /* font-size: 16px; */
   font-size: 12px;
 `;
