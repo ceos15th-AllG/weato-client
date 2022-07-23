ed16225ab1bfb86eab00485f8e3fa500ceb016bb
poodlepoodle <chammal97@naver.com>
2022-07-23 17:13:29 +0900

    :lipstick: style: 카드 컴포넌트 스타일링까지 완료
    카드 컴포넌트 레이아웃 및 스타일까지 완료

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,9 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import { jsx, css } from '@emotion/react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { gray07 } from '../../../styles/Colors';
 
-const CardStyle = css`
-  width: 200px;
-  height: 200px;
+import { Headline1, Body2 } from '../../../styles/FontStyle';
+
+import Tag from './Tag';
+
+const CardLayout = styled.section`
+  width: 240px;
+  height: 240px;
 
   border-radius: 16px;
 
@@ -11,34 +17,48 @@ const CardStyle = css`
   flex-direction: column;
   justify-content: space-between;
 
-  padding: 32px;
+  padding: 46px;
 
-  background-color: orange;
+  line-height: 26px;
+
+  box-shadow: -4px -4px 12px rgba(0, 0, 0, 0.03),
+    3.2px 3.2px 12px rgba(0, 0, 0, 0.06);
 `;
 
-const CardTopContainer = css`
+const CardText = styled.article`
   display: flex;
   justify-content: flex-start;
   align-items: flex-start;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 `;
 
-const CardBottomContainer = css`
+const CardBottomRow = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: left;
+`;
+
+const CardDate = styled.time`
+  display: flex;
+
+  color: ${gray07};
 `;
 
 const Card = ({ text, date, tag }) => {
   return (
-    <div css={CardStyle}>
-      <div css={CardTopContainer}>
-        <div>{text}</div>
-      </div>
+    <CardLayout>
+      <CardText css={Headline1}>{text}</CardText>
 
-      <div css={CardBottomContainer}>
-        <div>{date}</div>
-        <div>{tag}</div>
-      </div>
-    </div>
+      <CardBottomRow>
+        <CardDate css={Body2}>{date}</CardDate>
+        <Tag text={tag} />
+      </CardBottomRow>
+    </CardLayout>
   );
 };
 
