8e12f02c2f88a36bae1e7a6e6ea7059339944b58
poodlepoodle <chammal97@naver.com>
2022-07-25 11:05:13 +0900

    :white_check_mark: test: 컴포넌트 테스트 페이지 새로 추가
    컴포넌트 단위 개발 상황 공유를 위한 /componentsample 페이지 임시로 추가

---
+++ pages/componentsample/index.jsx
@@ -0,0 +1,16 @@
+export default function ComponentSample() {
+  return (
+    <div>
+      <main>
+        <h1>컴포넌트 샘플 페이지</h1>
+        <h3>
+          홈페이지 : <a href="/">weato.net</a>
+        </h3>
+        <h4>
+          임시로 디자인시스템에서 구현한 컴포넌트들을 띄워주고 테스팅하기 위한
+          화면입니다.
+        </h4>
+      </main>
+    </div>
+  );
+}
