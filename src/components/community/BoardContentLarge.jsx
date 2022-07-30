45bc905d3c5e5a214d4a339a5a36ffe0bb3fab13
poodlepoodle <chammal97@naver.com>
2022-07-30 16:20:59 +0900

    :hammer: chore: 커뮤니티 홈 페이지 내 큰 게시판 컴포넌트 분리
    posts 내용을 props 형태로 BoardContentLarge.jsx로 전달하도록 분리

---
+++ src/components/community/BoardContentLarge.jsx
@@ -0,0 +1,95 @@
+import styled from '@emotion/styled';
+
+import Image from 'next/image';
+
+import Tag from './PostTag';
+
+import { Subhead3, Body1 } from '../../../styles/FontStyle';
+
+import { gray05, text_black } from '../../../styles/Colors';
+
+import icon_views from '../../../public/icon_gray_views.png';
+
+const Layout = styled.div`
+  height: 352px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Title = styled.span`
+  width: 475px;
+
+  margin-left: 44px;
+
+  ${Subhead3}
+
+  color : ${text_black};
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+const Hashtag = styled.span`
+  margin-right: 57px;
+
+  ${Body1}
+  text-align : right;
+
+  color: ${text_black};
+`;
+
+const Views = styled.span`
+  width: 37.46px;
+
+  ${Body1}
+  text-align : right;
+
+  color: ${gray05};
+`;
+
+const Date = styled.span`
+  width: 111px;
+
+  ${Body1}
+  text-align : right;
+
+  color: ${gray05};
+`;
+
+function BoardContentLarge({ posts }) {
+  return (
+    <Layout>
+      {posts.map(({ category, title, comments, tag, view, date }) => (
+        <Box>
+          <Box>
+            <Tag text={category} />
+            <Title>
+              {title} [{comments}]
+            </Title>
+          </Box>
+          <Box>
+            <Hashtag>#{tag}</Hashtag>
+            <Box>
+              <Image src={icon_views} width={17.54} height={12} />
+              <Views>{view}</Views>
+            </Box>
+            <Date>{date}</Date>
+          </Box>
+        </Box>
+      ))}
+    </Layout>
+  );
+}
+
+export default BoardContentLarge;
