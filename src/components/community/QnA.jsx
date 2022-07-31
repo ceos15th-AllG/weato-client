266b1cc852b181702dc9699ecb714ed55f442295
poodlepoodle <chammal97@naver.com>
2022-07-31 16:58:34 +0900

    :sparkles: feat: 커뮤니티 홈 페이지 하단 QnA 펼치기 기능 구현
    useState로 전달받은 active 값으로 조건 부 렌더링 수행

--- src/components/community/QnA.jsx
+++ src/components/community/QnA.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import BoardHeader from './BoardHeader';
 
@@ -64,68 +64,73 @@ const Answer = styled.span`
 `;
 
 const QnA = () => {
-  const { qnaActive, setQnaActive } = useState([
-    true,
-    false,
-    false,
-    false,
-    true,
-  ]);
-  const qnas = [
+  const [qnas, setQnas] = useState([
     {
+      id: 0,
       question: '커뮤니티에는 어떤 글들을 공유하나요?',
       answer: '아무 글',
-      active: false,
-    },
-    {
-      question: '어디까지 제 경험을 공유할 수 있는 건가요?',
-      answer: '전부 ㄱ',
       active: true,
     },
     {
+      id: 1,
+      question: '어디까지 제 경험을 공유할 수 있는 건가요?',
+      answer: '전부 ㄱ',
+      active: false,
+    },
+    {
+      id: 2,
       question: '어떤 형태로 게시글을 작성하면 될까요?',
       answer: '맘대로',
       active: false,
     },
     {
+      id: 3,
       question: '포인트는 어떻게 모을 수 있나요?',
       answer: '열심히',
       active: false,
     },
     {
+      id: 4,
       question: '등급이 높으면 어떤 혜택이 있나요?',
       answer: '질문',
       active: false,
     },
-  ];
+  ]);
 
-  console.log('변수의 상태', qnaActive);
+  const toggleActive = (id) => {
+    setQnas(
+      qnas.map((qna) =>
+        // qna.index === index ? { ...qna, active: !qna.active } : qna
+        qna.id === id ? { ...qna, active: !qna.active } : qna
+      )
+    );
+  };
 
   return (
     <Layout>
       <BoardHeader title={'Q&A'} />
 
-      {qnas.map(({ question, answer, active, index }) =>
+      {qnas.map(({ id, question, answer, active }) =>
         active ? (
-          <>
-            <MenuOpened key={index}>
-              <Question>Q. 사랑해요 솔빙~~~~</Question>
+          <div key={id}>
+            <MenuOpened>
+              <Question>Q. {question}</Question>
               <BtnMore
-                onClick={(event) => {
-                  alert(event);
+                onClick={() => {
+                  toggleActive(id);
                 }}
               >
                 ^ v ^
               </BtnMore>
             </MenuOpened>
-            <Answer>나두 사랑해요~~~</Answer>
-          </>
+            <Answer>{answer}</Answer>
+          </div>
         ) : (
-          <MenuClosed key={index}>
+          <MenuClosed key={id}>
             <Question>Q. {question}</Question>
             <BtnMore
-              onClick={(event) => {
-                alert(event);
+              onClick={() => {
+                toggleActive(id);
               }}
             >
               답변 보기 &#xE001;
