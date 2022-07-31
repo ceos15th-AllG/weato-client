601ba0c198fb114b2a724dffffbbf79ecb9c36e9
poodlepoodle <chammal97@naver.com>
2022-08-01 02:42:23 +0900

    :sparkles: feat: 커뮤니티 페이지 내 탭 바 컴포넌트 분리
    props로 전달받은 값에 따라 하이라이트 스타일로 렌더링하도록 분리

---
+++ src/components/community/TabBar.jsx
@@ -0,0 +1,169 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import Button from '../common/ButtonContainer';
+
+import { Headline1, Subhead4 } from '../../../styles/FontStyle';
+
+import { main, gray02, gray05, text_black } from '../../../styles/Colors';
+
+const Layout = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  border-bottom: 2px solid ${gray02};
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderTextSelected = styled.span`
+  ${Headline1}
+
+  margin-top : 5px;
+  padding-bottom: 9px;
+
+  color: ${main};
+
+  border-bottom: 4px solid ${main};
+`;
+
+const HeaderText = styled.span`
+  ${Headline1}
+
+  margin-top : 5px;
+  padding-bottom: 9px;
+
+  color: ${gray05};
+
+  border-bottom: 4px solid transparent;
+`;
+
+const DropDown = styled.div`
+  width: 92px;
+
+  margin-left: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${Subhead4}
+
+  color : ${text_black};
+`;
+
+function TabBar({ selected }) {
+  if (selected === 'hot') {
+    return (
+      <Layout>
+        <Box>
+          <HeaderTextSelected
+            css={css`
+              margin-left: 11px;
+            `}
+          >
+            핫토픽
+          </HeaderTextSelected>
+          <HeaderText
+            css={css`
+              margin-left: 123px;
+            `}
+          >
+            나만의 관리법
+          </HeaderText>
+          <HeaderText
+            css={css`
+              margin-left: 132px;
+            `}
+          >
+            질문
+          </HeaderText>
+        </Box>
+        <Box>
+          <Button text="글쓰기" btnType="7" />
+          <DropDown>
+            <span>태그</span>
+          </DropDown>
+        </Box>
+      </Layout>
+    );
+  } else if (selected === 'knowhow') {
+    return (
+      <Layout>
+        <Box>
+          <HeaderText
+            css={css`
+              margin-left: 11px;
+            `}
+          >
+            핫토픽
+          </HeaderText>
+          <HeaderTextSelected
+            css={css`
+              margin-left: 123px;
+            `}
+          >
+            나만의 관리법
+          </HeaderTextSelected>
+          <HeaderText
+            css={css`
+              margin-left: 132px;
+            `}
+          >
+            질문
+          </HeaderText>
+        </Box>
+        <Box>
+          <Button text="글쓰기" btnType="7" />
+          <DropDown>
+            <span>태그</span>
+          </DropDown>
+        </Box>
+      </Layout>
+    );
+  } else if (selected === 'questions') {
+    return (
+      <Layout>
+        <Box>
+          <HeaderText
+            css={css`
+              margin-left: 11px;
+            `}
+          >
+            핫토픽
+          </HeaderText>
+          <HeaderText
+            css={css`
+              margin-left: 123px;
+            `}
+          >
+            나만의 관리법
+          </HeaderText>
+          <HeaderTextSelected
+            css={css`
+              margin-left: 132px;
+            `}
+          >
+            질문
+          </HeaderTextSelected>
+        </Box>
+        <Box>
+          <Button text="글쓰기" btnType="7" />
+          <DropDown>
+            <span>태그</span>
+          </DropDown>
+        </Box>
+      </Layout>
+    );
+  }
+}
+
+export default TabBar;
