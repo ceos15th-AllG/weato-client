a7cac0699db8d77d861b06672d36c65a1ab01b8a
poodlepoodle <chammal97@naver.com>
2022-08-02 14:33:04 +0900

    :sparkles: feat: 마이 페이지 내 작성글 페이지 컴포넌트로 분리
    CommunityTab.jsx로 분리하고 레이아웃 및 스타일링 적용 완료

---
+++ src/components/mypage/BoardRow.jsx
@@ -0,0 +1,103 @@
+import styled from '@emotion/styled';
+
+import Image from 'next/image';
+
+import { Subhead3, Body1, Body3, Tag1 } from '../../../styles/FontStyle';
+
+import { gray05, gray06, text_black, tag_etc } from '../../../styles/Colors';
+
+import profile_sample from '../../../public/profile_sample.png';
+import icon_gray_views from '../../../public/icon_gray_views.png';
+import icon_color_heart from '../../../public/icon_color_heart.png';
+
+const Layout = styled.div`
+  margin-bottom: 22px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const SmallText = styled.span`
+  ${Subhead3}
+
+  color:  ${gray05};
+`;
+
+const Title = styled.span`
+  margin-top: 10px;
+
+  ${Body3}
+
+  color:  ${text_black};
+`;
+
+const Row = styled.div`
+  margin: 20px 0px;
+  padding-bottom: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-bottom: 1px solid ${tag_etc};
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Level = styled.span`
+  margin-left: 13px;
+
+  ${Body1}
+
+  color: ${gray06};
+`;
+
+const Name = styled.span`
+  margin-left: 6px;
+
+  ${Tag1}
+
+  color: ${gray05};
+`;
+
+const SmallBox = styled.div`
+  width: 52px;
+
+  margin-left: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+function BoardRow({ category, title, view, like, name, level }) {
+  return (
+    <Layout>
+      <SmallText>{category}</SmallText>
+      <Title>{title}</Title>
+      <Row>
+        <Box>
+          <Image src={profile_sample} width={24} height={24} />
+          <Level>{level}</Level>
+          <Name>{name}</Name>
+        </Box>
+        <Box>
+          <SmallBox>
+            <Image src={icon_gray_views} width={20} height={13.69} />
+            <SmallText>{view}</SmallText>
+          </SmallBox>
+          <SmallBox>
+            <Image src={icon_color_heart} width={16} height={14} />
+            <SmallText>{like}</SmallText>
+          </SmallBox>
+        </Box>
+      </Row>
+    </Layout>
+  );
+}
+
+export default BoardRow;
--- src/components/mypage/CommunityTab.jsx
+++ src/components/mypage/CommunityTab.jsx
@@ -1,15 +1,194 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
+import Link from 'next/link';
+
+import BoardRow from './BoardRow';
+import Pagenator from '../common/Pagenator';
+
+import {
+  Display1,
+  Headline1,
+  Subhead4,
+  Body3,
+} from '../../../styles/FontStyle';
+
+import { gray04, gray06, text_black } from '../../../styles/Colors';
+
 const Layout = styled.div`
-  margin: 0px 300px;
+  margin: 42px 300px 151px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.header`
+  margin-bottom: 43px;
+
+  ${Display1}
+
+  color : ${text_black};
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const SubHeader = styled.div`
+  width: 100%;
+  height: 80px;
+
+  display: flex;
+  align-items: center;
+
+  ${Headline1}
+
+  color : ${text_black};
+
+  border-top: 3px solid ${gray06};
+  border-bottom: 1px solid ${gray04};
+`;
+
+const InfoBox = styled.section`
+  margin-top: 38px;
+
+  display: flex;
+  align-items: center;
+`;
+
+const InfoName = styled.div`
+  width: 224px;
+
+  display: flex;
+  justify-content: flex-start;
+
+  ${Body3}
+
+  color : ${text_black};
+`;
+
+const InfoData = styled.div`
+  ${Subhead4}
+
+  color : ${text_black};
 
   display: flex;
 `;
 
+const Board = styled.div`
+  margin-top: 23px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const CenterBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 function CommunityTab() {
+  const basicData = {
+    level: '새싹',
+    currentPoint: 20,
+    nextLevel: 100,
+  };
+
+  const communityData = [
+    {
+      id: '0',
+      category: '관리법',
+      title: '아토피 이제 괜찮아졌어요',
+      view: '200',
+      like: '200',
+      name: '아토랑',
+      level: '새싹',
+    },
+    {
+      id: '1',
+      category: '질문',
+      title: '아토피 이제 괜찮아졌어요',
+      view: '200',
+      like: '200',
+      name: '아토랑',
+      level: '새싹',
+    },
+    {
+      id: '2',
+      category: '관리법',
+      title: '아토피 이제 괜찮아졌어요',
+      view: '200',
+      like: '200',
+      name: '아토랑',
+      level: '새싹',
+    },
+    {
+      id: '3',
+      category: '질문',
+      title: '아토피 이제 괜찮아졌어요',
+      view: '200',
+      like: '200',
+      name: '아토랑',
+      level: '새싹',
+    },
+    {
+      id: '4',
+      category: '관리법',
+      title: '아토피 이제 괜찮아졌어요',
+      view: '200',
+      like: '200',
+      name: '아토랑',
+      level: '새싹',
+    },
+  ];
+
   return (
     <Layout>
-      <span>dd</span>
+      <Row>
+        <Header>회원정보</Header>
+      </Row>
+
+      <SubHeader>기본 정보</SubHeader>
+      <InfoBox>
+        <InfoName>레벨</InfoName>
+        <InfoData>{basicData.level}</InfoData>
+      </InfoBox>
+      <InfoBox>
+        <InfoName>포인트</InfoName>
+        <InfoData>
+          {basicData.currentPoint}/{basicData.nextLevel}
+        </InfoData>
+      </InfoBox>
+
+      <SubHeader
+        css={css`
+          margin-top: 149px;
+        `}
+      >
+        나의 작성글
+      </SubHeader>
+      <Board>
+        {communityData.map(
+          ({ id, category, title, view, like, name, level }) => (
+            <BoardRow
+              key={id}
+              category={category}
+              title={title}
+              view={view}
+              like={like}
+              name={name}
+              level={level}
+            />
+          )
+        )}
+      </Board>
+
+      <CenterBox>
+        <Pagenator />
+      </CenterBox>
     </Layout>
   );
 }
