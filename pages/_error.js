f2c064621bf854ecc944b88b8a9eb8fed3296146
poodlepoodle <chammal97@naver.com>
2022-08-04 18:36:12 +0900

    :sparkles: feat: 에러 페이지 추가
    400, 404, 500 코드에 따른 에러 페이지 추가

---
+++ pages/_error.js
@@ -0,0 +1,94 @@
+import styled from '@emotion/styled';
+
+import Button from '../src/components/common/ButtonContainer';
+
+const Layout = styled.div`
+  height: calc(100vh - 340px);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ErrorCode = styled.span`
+  font-size: 200px;
+  font-weight: 700;
+
+  color: #797979;
+`;
+
+const ErrorText = styled.span`
+  margin-bottom: 37px;
+
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 42px;
+  text-align: center;
+
+  color: #797979;
+`;
+
+const Error400 = () => {
+  return (
+    <Box>
+      <ErrorCode>400</ErrorCode>
+      <ErrorText>
+        찾을 수 없는 페이지 입니다.
+        <br />
+        요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요.
+      </ErrorText>
+      <Button text="홈으로 이동" btnType="4" />
+    </Box>
+  );
+};
+
+const Error404 = () => {
+  return (
+    <Box>
+      <ErrorCode>404</ErrorCode>
+      <ErrorText>
+        요청하신 페이지를 찾을 수 없습니다.
+        <br />
+        요청하신 페이지가 사라졌거나, 잘못된 경로를 이용하셨어요.
+      </ErrorText>
+      <Button text="홈으로 이동" btnType="4" />
+    </Box>
+  );
+};
+
+const Error500 = () => {
+  return (
+    <Box>
+      <ErrorCode>500</ErrorCode>
+      <ErrorText>
+        웹 사이트에서 페이지를 표시할 수 없습니다.
+        <br />웹 사이트가 유지 관리 중이거나 프로그래밍 오류가 발생한 것으로
+        보입니다.
+      </ErrorText>
+      <Button text="홈으로 이동" btnType="4" />
+    </Box>
+  );
+};
+
+function Error({ statusCode }) {
+  return (
+    <Layout>
+      {statusCode === 400 ? <Error400 /> : undefined}
+      {statusCode === 404 ? <Error404 /> : undefined}
+      {statusCode === 500 ? <Error500 /> : undefined}
+    </Layout>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
