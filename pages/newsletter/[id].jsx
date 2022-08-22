2a888b9ccabda021fc9afe096e752ad02997c8f2
poodlepoodle <chammal97@naver.com>
2022-08-22 11:33:52 +0900

    :sparkles: feat: 뉴스레터 작성 도우미 폼 간단하게 제작
    일일히 치기 너무 귀찮아서 /newsletter/new에 배포

--- pages/newsletter/[id].jsx
+++ pages/newsletter/[id].jsx
@@ -43,6 +43,11 @@ const ContentHeader = styled.header`
 
   color : ${text_black};
   border-bottom: 3px solid ${gray06};
+
+  strong {
+    margin: 0px 6px;
+    font-weight: 300;
+  }
 `;
 
 const ButtonRow = styled.div`
@@ -103,6 +108,14 @@ const NewsletterForm = styled.div`
     text-align: left;
 
     color: ${text_black};
+
+    a {
+      color: ${main};
+    }
+
+    strong {
+      font-weight: 500;
+    }
   }
 
   .content-line {
@@ -155,25 +168,26 @@ function Newsletter(props) {
 
   const { newsletterData } = props;
 
-  const sampleNewsletterData =
-    "<header class='header'>샤워, 목욕, 세면, 습관으로 아토피 관리</header ><span class='header-subscription'>본 뉴스레터는 전문기자의 기사를 기반으로 작성되었습니다.</span><date class='header-date'>2022-05-25</date><span class='content-text'>기온이 높아지면서 찾아오기 시작한 여름.<br />땀이 나기 쉬운 환경의 계절이 함께 오면서 관리에 대해 신경써야 될 시간이 다가와 우려하시는 분들도 있을 것 같습니다.</span><span class='content-text'>그래서 세면에 대한 글을 통해 기존에 관리하시던 분들에게는 리마인더 역할을, 접한지 얼마 안 되는 분들에게는 정보를 제공하고자 합니다.</span><div class='content-line'></div><header class='content-header'>💦 간단하게 샤워로</header><span class='content-text'>과거에 잦은 샤워와 세면은 피부 건조를 일으킬 수 있어 제한하는 것을 권했지만,<br />오히려 최근 오염된 피부가 주는 악영향이 더 크므로 잦은 샤워를 권하고 있습니다.</span><span class='content-text'>전문가들은 아토피 환자들에게 <a class='content-a'>다음과 같은 내용들의 샤워 습관</a>을 권했습니다.</span><ul class='content-list'><li class='content-listitem'>미지근한 온도의 물</li><li class='content-listitem'>너무 길지 않게 15분 이내로 샤워 진행할 것</li><li class='content-listitem'>샤워 후 3분 이내에 전신에 보습제를 바를 것</li></ul><div class='content-line'></div><header class='content-header'>🛁 좀 더 세밀하게 목욕으로</header><span class='content-text'>목욕도 샤워와 마찬가지로 과거 연구에서는 피부 손상을 촉진시킨다고 생각해 목욕 횟수를 일주일에 1번으로 줄이는 것으로 치료를 권했습니다.</span><span class='content-text'>최근 연구는 이러한 내용과는 반대로 오히려 <a class='content-a'>목욕을 통한 위생 유지</a>가 아토피 증상 완화에 매우 중요하다는 결과가 나왔습니다. 과거의 연구 결과에 영향을 미친 것은 목욕 그 자체가 아닌 (지나치게 뜨거운 물, 때밀이 사용, 피부를 문질러 닦는 목욕 습관, 자극성 바디 소프 사용)인 것으로 지목되고 있네요. 이러한 아토피 환자들에게 권해지는 목욕 방법은 다음과 같습니다.</span><ul class='content-list'><li class='content-listitem'>하루 1회의 목욕을 권장</li><li class='content-listitem'>땀이 많이 나는 여름에는 하루 2회까지도 권장</li><li class='content-listitem'>중성이나 약산성의 저자극성 세정제를 사용 권장</li><li class='content-listitem'>때를 미는 등의 피부를 자극하는 목욕 습관은 피할 것</li><li class='content-listitem'>목욕 끝난 후 10분 내 바로 보습제를 전신에 바를 것</li></ul><div class='content-line'></div><span class='content-text'>샤워도, 목욕도 피부를 세정해주는 효과는 있지만 둘 다 모두 마무리 후 보습제를 통한 보습을 중요하게 언급하고 있는 만큼, 마무리 후 보습에도 신경을 써야 하겠습니다. 마지막으로 최근 아토피 환자 분들을 위해 <a class='content-a'>충남 수안보 온천이 제공하는 혜택</a>에 대해 간단히 말씀드리며 참고가 되기를 바랍니다.</span><ul class='content-list'><li class='content-listitem'>대한 아토피 협회와 공무원 연금 공단이 수안보 온천에 대해 협약 체결</li><li class='content-listitem'>전국 아토피 환자들에게 저렴한 비용으로 수안보 온천 이용</li></ul>";
+  const sampleNewsletterData = `<header class='header'>늘고 있는 미세먼지와 아토피</header><span class='header-subscription'>본 뉴스레터는 전문기자의 기사를 기반으로 작성되었습니다.</span><date class='header-date'>2022-05-25</date><span class='content-text'>코로나가 발생하기 전부터, 이미 대한민국에서는 미세먼지가 많은 대기 환경으로 많은 사람들이 마스크에 익숙해져 있었습니다.</span><span class='content-text'>대기 중 미세먼지는 사람들의 호흡기와 피부에 악영향을 주기 때문에, 일반 사람들도 마스크를 쓰거나 외출 후 반드시 손을 씻는 등의 대응을 하고 있습니다.<br />최근에도 대한민국은 대기 중 미세먼지에 신경 쓸 수밖에 없는데 아토피 환우들에게는 어떠한 악영향을 주는지, 아토피 환우분들은 어떻게 대처해야 하는지에 대한 정보를 제공하고자 합니다.</span><div class='content-line'></div><header class='content-header'>😡 미세먼지가 미치는 악영향</header><span class='content-text'><a>순천향 의대 측의 연구 결과</a>에 따르면 미세먼지와 초미세먼지가 증가할 때마다 아토피 환자들의 월 평균 병원 방문 횟수도 같이 증가하였다고 합니다.<br />이는 <a>한국표준과학연구원에서 개발한 신기술</a>로도 확인하였는데요, 대부분 각질이 손상된 아토피 환자들의 피부에는 미세먼지가 더 쉽게 침투해 염증이 악화되는 것이 드러났습니다.</span><span class='content-text'>게다가, 일반 환자들만이 아닌 임산부들의 경우에도 출산 전후 미세먼지에 노출된다면, 출산 후 아이가 생후 1년 사이 아토피를 갖게 될 확률이 타 임산부들보다 약 2배 높다는 연구 결과가 존재합니다.</span><div class='content-line'></div><header class='content-header'>❗️ 대처할 수 있는 방법</header><span class='content-text'>이러한 미세먼지에 대처할 수 있는 방법들로 다음의 사례들을 언급드릴 수 있겠는데요.</span><span class='content-text'><strong>1. 평소보다 많은 미세먼지의 대기에 외출했을 시, 빠르게 세면해주는 것</strong><br/>손, 얼굴 뿐만 아니라 전신을 다 세면해주는 것도 좋으며 그 중 목욕은 세면 영역도 넓고, 피부의 이물질을 제거해 주어 피부를 건강하게 유지하는 데에 도움을 줍니다.<br />다만, 목욕을 할 경우 다음과 같은 사항들에 주의를 기울여야 할 것 같습니다.</span><ul class='content-list'><li class='content-listitem'>pH 4.5~ 5.5의 약산성 세정제 사용</li><li class='content-listitem'>지나치게 오랜 시간 동안의 목욕은 금물</li></ul><span class='content-text'>목욕과 관련한 정보에 대해 정리한 뉴스레터가 따로 있으니 <a>여기서</a> 목욕에 대해 좀 더 참고하시면 도움이 되겠습니다.</span><strong>2. 최소 하루 2회 이상 보습제 도포</strong><br/><span class='content-text'>세면 시 뿐만 아니라, 세면이 안 되어 있는 상태에서도 보습제를 바르는 것은 도움이 된다고 합니다.<br />씻지 않은 상태에서의 보습제 도포가 세균의 증식을 유발한다는 증거가 없으므로 꼭 씻은 뒤가 아니더라도 자주 보습제를 발라주면 좋을 것 같습니다.<br />미국피부과의사회에 따르면 보습제의 주간 적정량 기준은 다음과 같습니다.</span><ul class='content-list'><li class='content-listitem'>소아 : 주당 100g 이상</li><li class='content-listitem'>성인 : 주당 250g 이상</li></ul><div class='content-line'></div><span class='content-text'>날마다 대기 내 미세먼지 농도를 확인하시면서 외출에 주의하시는 분들이 적지 않을 것으로 보입니다.</span><span class='content-text'>미세먼지 뿐만이 아니라 다른 상황들도 주의하셔야 하는 아토피 환우 분들이, 정보를 찾는 데에 들이시는 시간과 비용이 줄기를 바라며 오늘 하루도 힘내시기를 기원합니다!</span>`;
 
   return (
     <Layout ref={ref}>
       <Head>
         <title>뉴스레터 - {newsletterData.title}</title>
+        {/* <title>뉴스레터 - 샤워, 목욕, 세면, 습관으로 아토피 관리</title> */}
       </Head>
 
       <ProgressBar percentage={percentage * 100} />
 
       <Content>
         <ContentHeader>
-          뉴스레터 &#xE001; {dict[newsletterData.tagType]}
+          뉴스레터<strong>&#xE001;</strong>
+          {dict[newsletterData.tagType]}
         </ContentHeader>
 
         <NewsletterForm
-          dangerouslySetInnerHTML={{ __html: newsletterData.content }}
           // dangerouslySetInnerHTML={{ __html: sampleNewsletterData }}
+          dangerouslySetInnerHTML={{ __html: newsletterData.content }}
         />
       </Content>
 
---
+++ pages/newsletter/new.jsx
@@ -0,0 +1,340 @@
+import styled from '@emotion/styled';
+
+import { useState } from 'react';
+
+import Button from '@common/ButtonContainer';
+
+import { Display1, Subhead3, Headline1, Body2 } from '@styles/FontStyle';
+import {
+  main,
+  gray02,
+  gray05,
+  gray06,
+  gray07,
+  text_black,
+  tag_etc,
+} from '@styles/Colors';
+
+const Layout = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 100px 50px;
+`;
+
+const Row = styled.div`
+  width: 100%;
+  display: flex;
+
+  &:nth-child(2) {
+    margin-top: 100px;
+  }
+
+  & > span {
+    position: relative;
+    left: 100px;
+    font-size: 32px;
+    font-weight: 500;
+  }
+`;
+
+const Section = styled.section`
+  width: 80vw;
+  height: 1000px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 0px 50px;
+
+  & > span {
+    margin-bottom: 20px;
+
+    font-size: 20px;
+    color: ${main};
+    font-weight: 500;
+  }
+`;
+
+const ButtonArea = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+
+  font-size: 18px;
+
+  margin: 30px 0px;
+  padding: 24px;
+
+  border: none;
+  outline: none;
+
+  border-radius: 8px;
+  border: 1px solid black;
+`;
+
+const NewsletterForm = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 20px;
+
+  border-radius: 8px;
+  border: 1px solid black;
+
+  overflow: scroll;
+
+  .header {
+    width: 100%;
+
+    margin-bottom: 24px;
+
+    ${Display1};
+
+    text-align: center;
+
+    color: ${text_black};
+  }
+
+  .header-subscription {
+    width: 100%;
+
+    margin-bottom: 28px;
+
+    ${Body2};
+
+    text-align: center;
+
+    color: ${gray05};
+  }
+
+  .header-date {
+    width: 100%;
+
+    margin-bottom: 27px;
+
+    ${Body2};
+
+    text-align: right;
+
+    color: ${gray07};
+  }
+
+  .content-text {
+    margin-bottom: 24px;
+
+    ${Body2};
+
+    line-height: 24px;
+    text-align: left;
+
+    color: ${text_black};
+
+    a {
+      color: ${main};
+    }
+
+    strong {
+      font-weight: 500;
+    }
+  }
+
+  .content-line {
+    width: 100%;
+
+    margin: 16px 0px 40px;
+
+    border-top: 1px solid ${tag_etc};
+  }
+
+  .content-header {
+    margin-bottom: 20px;
+
+    ${Headline1};
+
+    color: ${text_black};
+  }
+
+  .content-a {
+    color: ${main};
+  }
+
+  .content-list {
+    margin-bottom: 24px;
+
+    list-style: disc outside none;
+    line-height: 28px;
+  }
+
+  .content-listitem {
+    list-style: disc outside none;
+  }
+`;
+
+function New() {
+  const [newsletter, setNewsletter] = useState(``);
+  const [cursorPosition, setCursorPosition] = useState(0);
+  const [result, setResult] = useState(``);
+
+  const onChange = (event) => {
+    setNewsletter(event.target.value);
+  };
+
+  const onCursorMove = (event) => {
+    setCursorPosition(event.target.selectionStart);
+  };
+
+  const onConvert = () => {
+    let convertResult = '';
+    const lines = newsletter.split('\n');
+    for (let line of lines) {
+      convertResult += line.trim();
+    }
+    setResult(convertResult);
+  };
+
+  const insertText = (option) => {
+    const newText = {
+      'header-box': `<header class='header'>
+    뉴스레터 제목
+</header>
+<span class='header-subscription'>
+    본 뉴스레터는 전문기자의 기사를 기반으로 작성되었습니다.
+</span>
+<date class='header-date'>
+    2022.00.00
+</date>
+`,
+      'content-header': `<div class='content-header'>
+    소제목
+</div>      
+`,
+      'content-text': `<span class='content-text'>
+    문단 내용
+</span>      
+`,
+      'content-a': `<a>링크</a>
+`,
+      'content-strong': `<strong>강조문</strong>
+`,
+      'content-br': `<br />    
+`,
+      'content-line': `<div class='content-line'></div>      
+`,
+      'content-list': `<ul class='content-list'>
+    <li class='content-listitem'>리스트 아이템 1</li>
+    <li class='content-listitem'>리스트 아이템 2</li>
+    <li class='content-listitem'>리스트 아이템 3</li>
+</ul>      
+`,
+    };
+    // let frontText = newsletter.substring(0, cursorPosition);
+    // let backText = newsletter.substring(cursorPosition, newsletter.length);
+    // setNewsletter(frontText + newText[option] + backText);
+    // setCursorPosition(cursorPosition + newText[option].length);
+    setNewsletter(newsletter + '\n' + newText[option]);
+  };
+
+  return (
+    <Layout>
+      <Row>
+        <span>쉽게 쉽게 일합시다</span>
+      </Row>
+      <Row>
+        <Section>
+          <ButtonArea>
+            <Button
+              text="제목 박스"
+              btnType="1"
+              onClick={() => {
+                insertText('header-box');
+              }}
+            />
+            <Button
+              text="소제목"
+              btnType="1"
+              onClick={() => {
+                insertText('content-header');
+              }}
+            />
+            <Button
+              text="문단"
+              btnType="1"
+              onClick={() => {
+                insertText('content-text');
+              }}
+            />
+            <Button
+              text="링크"
+              btnType="1"
+              onClick={() => {
+                insertText('content-a');
+              }}
+            />
+            <Button
+              text="강조"
+              btnType="1"
+              onClick={() => {
+                insertText('content-strong');
+              }}
+            />
+            <Button
+              text="리스트"
+              btnType="1"
+              onClick={() => {
+                insertText('content-list');
+              }}
+            />
+            <Button
+              text="줄바꿈"
+              btnType="1"
+              onClick={() => {
+                insertText('content-br');
+              }}
+            />
+            <Button
+              text="구분선"
+              btnType="1"
+              onClick={() => {
+                insertText('content-line');
+              }}
+            />
+          </ButtonArea>
+          <TextArea
+            placeholder="뉴스레터 내용을 입력하세요"
+            value={newsletter}
+            onChange={onChange}
+            onKeyUp={onCursorMove}
+          />
+          <Button text="한 줄로 변환하기" btnType="4" onClick={onConvert} />
+        </Section>
+        <Section>
+          <span>아래 화면처럼 보여요</span>
+          <NewsletterForm dangerouslySetInnerHTML={{ __html: newsletter }} />
+        </Section>
+      </Row>
+
+      <Row>
+        <TextArea
+          placeholder="한 줄로 변환된 뉴스레터 내용입니다"
+          value={result}
+        />
+      </Row>
+    </Layout>
+  );
+}
+
+export default New;
