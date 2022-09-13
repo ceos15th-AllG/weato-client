9006c9dd41f9e35f857af719324b961133a0381e
poodlepoodle <chammal97@naver.com>
2022-09-13 14:19:19 +0900

    :lipstick: style: 뉴스레터 통합 관리 어드민 페이지
    레이아웃 잡는 중...

--- pages/newsletter/admin.jsx
+++ pages/newsletter/admin.jsx
@@ -1,15 +1,46 @@
 import styled from '@emotion/styled';
 
-import { useState } from 'react';
+import { useState, useEffect, useContext } from 'react';
 
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-import { Body2 } from '@styles/FontStyle';
-import { gray03 } from '@styles/Colors';
+import axios from 'axios';
+
+import Context from '@contexts/Context';
+
+import Button from '@common/ButtonContainer';
+import Tag from '@common/Tag';
+
+import {
+  Display1,
+  Headline1,
+  Headline2,
+  Subhead3,
+  Body2,
+  Subhead4,
+} from '@styles/FontStyle';
+import {
+  main,
+  gray03,
+  gray05,
+  gray07,
+  text_black,
+  tag_etc,
+} from '@styles/Colors';
 
 import icon_menu from '@public/newsletter/icon_menu.png';
 import icon_search from '@public/icon_search.png';
+
+const dict = {
+  DRUG: '약품',
+  SLEEP: '수면',
+  CLEANING: '세면',
+  FOOD: '음식',
+  ENVIRONMENT: '환경',
+  OTHERWISE: '기타',
+  ALL: '전체',
+};
 
 const Layout = styled.div`
   width: 100%;
@@ -22,8 +53,11 @@ const Layout = styled.div`
 `;
 
 const Sidebar = styled.div`
-  width: ${({ open }) => (open ? `480px` : `0`)};
+  width: ${({ open }) => (open ? `600px` : `0`)};
   height: 100%;
+
+  position: relative;
+  z-index: 2500;
 
   display: flex;
   flex-direction: column;
@@ -31,7 +65,7 @@ const Sidebar = styled.div`
 
   overflow: hidden;
 
-  padding: 120px 0px;
+  padding: 120px 0px 30px;
 
   background-color: white;
 
@@ -50,6 +84,7 @@ const SearchInputContainer = styled.div`
   border-radius: 4px;
 
   padding: 0px 16px;
+  margin-bottom: 40px;
 `;
 
 const SearchInput = styled.input`
@@ -65,6 +100,8 @@ const SearchInput = styled.input`
 `;
 
 const Content = styled.div`
+  position: relative;
+
   width: 100%;
   height: 100%;
 
@@ -80,13 +117,273 @@ const IconContainer = styled.div`
   left: 28px;
 `;
 
+const SideBarMenuBox = styled.div`
+  width: 80%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  overflow: scroll;
+
+  .title {
+    ${Subhead4}
+    margin-bottom : 12px;
+  }
+
+  .item {
+    width: 100%;
+    height: 55px;
+
+    border-radius: 8px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 6px;
+    padding: 0px 20px;
+
+    background-color: #fafafa;
+
+    &:hover {
+      background-color: #e4e4e4;
+    }
+
+    transition: background-color 0.3s ease;
+
+    span {
+      ${Subhead3}
+
+      font-weight : 400;
+      color: ${text_black};
+
+      width: 75%;
+
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+    }
+  }
+`;
+
+const Row = styled.div`
+  width: 100%;
+  display: flex;
+
+  padding-bottom: 60px;
+
+  ${({ center }) => (!center ? `` : `justify-content : center;`)};
+  ${({ top }) => (!top ? `` : `width : 500px; margin-left : 100px`)};
+
+  & > span {
+    position: relative;
+    left: 100px;
+    font-size: 32px;
+    font-weight: 500;
+  }
+`;
+
+const Section = styled.section`
+  width: 80vw;
+  height: 1000px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 0px 50px;
+
+  & > span {
+    margin-bottom: 20px;
+
+    font-size: 20px;
+    color: ${main};
+    font-weight: 500;
+  }
+`;
+
+const ButtonArea = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 100%;
+  resize: none;
+
+  font-size: 18px;
+
+  margin: 30px 0px;
+  padding: 24px;
+
+  border: none;
+  outline: none;
+
+  border-radius: 8px;
+`;
+
+const NewsletterForm = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 20px;
+
+  border-radius: 8px;
+
+  background-color: white;
+
+  overflow: scroll;
+
+  .header {
+    width: 100%;
+
+    margin-bottom: 24px;
+
+    ${Display1};
+
+    text-align: center;
+
+    color: ${text_black};
+  }
+
+  .header-subscription {
+    width: 100%;
+
+    margin-bottom: 28px;
+
+    ${Body2};
+
+    text-align: center;
+
+    color: ${gray05};
+  }
+
+  .header-date {
+    width: 100%;
+
+    margin-bottom: 27px;
+
+    ${Body2};
+
+    text-align: right;
+
+    color: ${gray07};
+  }
+
+  .content-text {
+    margin-bottom: 24px;
+
+    ${Body2};
+
+    line-height: 24px;
+    text-align: left;
+
+    color: ${text_black};
+
+    a {
+      color: ${main};
+    }
+
+    strong {
+      font-weight: 500;
+    }
+  }
+
+  .content-line {
+    width: 100%;
+
+    margin: 16px 0px 40px;
+
+    border-top: 1px solid ${tag_etc};
+  }
+
+  .content-header {
+    margin-bottom: 20px;
+
+    ${Headline1};
+
+    color: ${text_black};
+  }
+
+  .content-a {
+    color: ${main};
+  }
+
+  .content-list {
+    margin-bottom: 24px;
+
+    list-style: disc outside none;
+    line-height: 28px;
+  }
+
+  .content-listitem {
+    list-style: disc outside none;
+  }
+`;
+
 const Admin = () => {
   const router = useRouter();
+  const [login, user, token] = useContext(Context);
+
   const [sideOpen, setSideOpen] = useState(false);
+
+  //   const [number, setNumber] = useState(null);
+
+  const [loaded, setLoaded] = useState(null);
+  const [loadedNewsletters, setLoadedNewsletters] = useState([]);
+
+  const [newsletter, setNewsletter] = useState(``);
 
   const onClick = (event) => {
     setSideOpen(!sideOpen);
   };
+
+  const onChange = (event) => {
+    setNewsletter(event.target.value);
+  };
+
+  const loadNewsletterLists = async (event) => {
+    let page = 1;
+    let buffer = [];
+
+    try {
+      while (true) {
+        const response = await axios({
+          method: 'get',
+          url: `https://www.weato.kro.kr/api/newsletters?page=${page}`,
+          // headers: {
+          //   Authorization: `Bearer ${token}`,
+          // },
+        });
+
+        buffer.push(...response.data.data);
+        page++;
+
+        if (page > response.data.max) {
+          break;
+        }
+      }
+
+      setLoadedNewsletters(buffer);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+      alert('서버 요청이 불가능하네요...');
+    }
+  };
+
+  useEffect(() => {
+    loadNewsletterLists();
+  }, []);
 
   return (
     <Layout>
@@ -99,9 +396,96 @@ const Admin = () => {
           <Image src={icon_search} width={24} height={24} />
           <SearchInput placeholder="뉴스레터 검색어 필터링..." />
         </SearchInputContainer>
+
+        <SideBarMenuBox>
+          {loaded ? (
+            <>
+              <span className="title">
+                뉴스레터 목록 ({loadedNewsletters.length})
+              </span>
+
+              {loadedNewsletters.map(({ title, tagType }, index) => (
+                <div className="item" key={index}>
+                  <Tag text={dict[tagType]} />
+                  <span>{title}</span>
+                </div>
+              ))}
+            </>
+          ) : undefined}
+        </SideBarMenuBox>
       </Sidebar>
 
-      <Content></Content>
+      <Content>
+        <Section>
+          <ButtonArea>
+            <Button
+              text="제목 박스"
+              btnType="1"
+              onClick={() => {
+                insertText('header-box');
+              }}
+            />
+            <Button
+              text="소제목"
+              btnType="1"
+              onClick={() => {
+                insertText('content-header');
+              }}
+            />
+            <Button
+              text="문단"
+              btnType="1"
+              onClick={() => {
+                insertText('content-text');
+              }}
+            />
+            <Button
+              text="링크"
+              btnType="1"
+              onClick={() => {
+                insertText('content-a');
+              }}
+            />
+            <Button
+              text="강조"
+              btnType="1"
+              onClick={() => {
+                insertText('content-strong');
+              }}
+            />
+            <Button
+              text="리스트"
+              btnType="1"
+              onClick={() => {
+                insertText('content-list');
+              }}
+            />
+            <Button
+              text="줄바꿈"
+              btnType="1"
+              onClick={() => {
+                insertText('content-br');
+              }}
+            />
+            <Button
+              text="구분선"
+              btnType="1"
+              onClick={() => {
+                insertText('content-line');
+              }}
+            />
+          </ButtonArea>
+          <TextArea
+            placeholder="뉴스레터 내용을 입력하세요"
+            value={newsletter}
+            onChange={onChange}
+          />
+        </Section>
+        <Section>
+          <span>아래 화면처럼 보여요</span>
+          <NewsletterForm dangerouslySetInnerHTML={{ __html: newsletter }} />
+        </Section>
+      </Content>
     </Layout>
   );
 };
